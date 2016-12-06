17b76066fca1c2623e16edfbdc7045cba6f6f491
shepard127 <dmitrij.budejkin@mail.ru>
2016-12-06 08:45:42 +0200

    fixed a TODO & comments

--- Problem Set 3/fifteen/fifteen.c
+++ Problem Set 3/fifteen/fifteen.c
@@ -17,7 +17,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>                 // для usleep()
+#include <unistd.h>                 // for usleep()
 
 // constants
 #define DIM_MIN 3
@@ -48,7 +48,7 @@ int main(int argc, string argv[])
 
     // ensure valid dimensions
     d = atoi(argv[1]);
-    if (d < DIM_MIN || d > DIM_MAX)             // проверяем что там пользователь ввел
+    if (d < DIM_MIN || d > DIM_MAX)             // check that the user entered there
     {
         printf("Board must be between %i x %i and %i x %i, inclusive.\n",
             DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
@@ -169,10 +169,10 @@ void init(void)
         row++;
     }
     
-    // выполнится в самом конце
+    //executed at the very end
     if(row == d)
     {
-        if((board[0][0] % 2) != 0)                                  // если наша матрица начинается на нечетный элемент - то 1 и 2 меняем местами
+        if((board[0][0] % 2) != 0)                                  // If our matrix begins at an odd item - the 1 and 2 change places
         {
             int replace = d - 1;
             int buff = board[replace][replace - 2];
@@ -233,9 +233,8 @@ void draw(void)
  */
 bool move(int tile)
 {
-    // TODO
-    /* 1 - определить текущую позицию tile */
-    /* 2 - если в диапазоне 1-й клетки от tile, есть 0 - то меняем tile с нолем местами */
+    /* 1 - determine the current position of the tile */
+    /* 2 - if the range of cells from the first tile, is zero - then change the tile with zero places */
     
     int row = 0, col =0;
     while(row < d)
@@ -294,7 +293,6 @@ bool move(int tile)
  */
 bool won(void)
 {
-    // TODO
     int row = 0, col = 0, testwon = 1;
     while (row < d)
     {
