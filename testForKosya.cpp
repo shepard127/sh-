2e97f4fce86d99dfc5ee11a8a4f670fff66d78e1
shepard127 <dmitrij.budejkin@mail.ru>
2017-02-23 14:34:26 +0200

    test_For_Kostya
    test_For_Kostya

---
+++ testForKosya.cpp
@@ -0,0 +1,42 @@
+
+const int SIZE = 20;
+int arrayWithtTwo[SIZE];
+
+int inputData(int* inputArray);
+int calcData(int* resultInArr);
+int printOutData(int* sumArray);
+
+int main()
+{
+	int* p_array = malloc(SIZE * sizeof(int));
+	
+	printOutData(calcData(p_array));
+	
+}
+
+int inputData(int* inputArray)
+{
+	printf("Заполните масссив цифрками");
+	// ну мы его типа заполнили
+	
+	return &inputArray[0]; // вернули адрес на первый элемент массива
+}
+
+int calcData(int* resultInArr)
+{
+	// в эту функцию мы получили уже заполненный массивчик
+	// мы тут типа выгребаем все двоички из массива и складываем их в другой массив arrayWithtTwo(он глобальный и его видим)
+	
+	return &arrayWithtTwo[0];		// возвращаем указатель на первый элемент массива с двоичками
+}
+
+int printOutData(int *sumArray)
+{
+	int i = 0;
+	while(i < SIZE)
+	{
+		printf("Сумма двоичек: %d", *(sumArray + i));
+		i++;
+	}	
+	
+}
